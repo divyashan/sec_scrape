52897970f61af2d8e8df2799ec688db1b4102d5d
Divya Shanmugam <divyashan@gmail.com>
2019-06-28 11:27:53 -0400

    csv to excel converter

---
+++ csv_to_excel.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import sys
+
+def csv_to_excel(csv_filename, excel_filename=None):
+    if not excel_filename:
+        excel_filename = csv_filename
+    writer = pd.ExcelWriter(excel_filename, engine='xlsxwriter')
+    csv_df = pd.read_csv(csv_filename)
+    csv_df.to_excel(writer, sheet_name='Sheet1')
+
+    print("Converted csv to excel file, saved at: ", excel_filename)
+
+if len(sys.argv) == 3:  
+    csv_to_excel(sys.argv[1], sys.argv[2])
+else:
+    print("Not enough arguments; are you running `python csv_to_excel.py <csv_file_name> <excel_file_name>`?")
